fc11441b3986ada9f3e8599d8dbeecc7ca1d6ebb
Frederico Pavao <frederico.pavao@ideastation.com>
2020-09-23 00:04:32 +0000

    Add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  {
+    path: "home",
+    component: HomeComponent,
+    loadChildren: () =>
+      import("./pages/home/home.module").then((m) => m.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
-
-
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
